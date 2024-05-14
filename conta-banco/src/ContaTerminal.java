ceaea045a7e461093ad7eb5dfccac91656a8cae0
fscamila21 <f.scamila@outlook.com>
2024-05-14 15:43:06 -0300

    Projeto Conta Banco - Trilha DIO

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Por favor, digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o número da agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Informe o seu nome completo: ");
+        String nome = scanner.next();
+
+        System.out.println("Informe o seu saldo: ");
+        double saldo = scanner.nextDouble();
+        
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco! Sua agência é " + agencia + ", conta " + numeroConta + ", e seu saldo de R$" + saldo + " já está disponível para saque.");
     }
 }
+
+
